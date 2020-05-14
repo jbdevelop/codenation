2000f67d9116b81ef254d9a5ab34e4bc998995b7
jbdevelop <jbernardorj@gmail.com>
2020-05-14 17:12:11 -0300

    Challenge Contacts App 2

---
+++ react-14/build/precache-manifest.0794ba2816750cabf2ccec786f69c448.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28671fd5e19398a8212cb97c75674a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6068c6d504175c266b51",
+    "url": "/static/css/main.a0e1ef8e.chunk.css"
+  },
+  {
+    "revision": "d8bc8438957951ab8ec9",
+    "url": "/static/js/2.e3431306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3431306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6068c6d504175c266b51",
+    "url": "/static/js/main.5a1ded2b.chunk.js"
+  },
+  {
+    "revision": "5f896162c57a64ede907",
+    "url": "/static/js/runtime-main.d96242e9.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/static/media/logo.82a7c062.svg"
+  }
+]);
